2fac3239b7492ccf211237a80bed28e16a48a79d
ChristopherTrimboli <christopher.trimboli@protonmail.com>
2021-05-12 21:59:54 -0700

    add App.tsx entry point

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+
+function App() {
+    return (
+        <div>
+            <h1>Own Me</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('react-container'))
