49db2f3f66a74c3516fd178704d0dd8b6a1c4582
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-13 21:47:16 -0700

    .babelrc file, show classNames in styled-components

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const TopNavContainer = styled.button`
+const TopNavContainer = styled.div`
     width: 100%;
     height: 50px;
 `;
@@ -9,7 +9,7 @@ const TopNavContainer = styled.button`
 export default function TopNav() {
     return (
         <TopNavContainer>
-            
+
         </TopNavContainer>
     );
 };
