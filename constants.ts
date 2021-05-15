eda64f2871af8cb6c1a5a82a198b9ada10a2b2b6
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-14 21:27:32 -0700

    add constants file, navLinks

---
+++ constants.ts
@@ -0,0 +1,19 @@
+interface navLink {
+    key: string,
+    title: string
+};
+
+const navLinks: Array<navLink> = [
+    {
+        key: "ourDream",
+        title: "Our Dream"
+    },
+    {
+        key: "getInvolved",
+        title: "Get Involved"
+    },
+    {
+        key: "$nude",
+        title: "$NUDE"
+    }
+];
