381b7cc933c8c731c2401b71725bb544d1aea6ca
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-15 23:51:19 -0700

    add applayout media query

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -3,6 +3,10 @@ import styled from "styled-components";
 
 const AppLayoutContainer = styled.div`
     padding: 50px 20%;
+
+    @media screen and (max-width: 992px) {
+        padding: 40px 5%;
+    }      
 `;
 
 export default function AppLayout({ children }) {
