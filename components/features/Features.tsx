9e359f1c2f88a3fe3f06eaa296716ba5ed07f70a
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-19 23:20:52 -0700

    map features card data

--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -9,13 +9,32 @@ const FeaturesContainer = styled.div`
     min-height: 800px;
 `;
 
+const featuresData = [
+    {
+        title: "NFT Encryption",
+        icon: padlock,
+        text: "State of the art NFT encryption methods make sure that your purchase stays private. Unlike other platforms, the content is not publicy viewable unless you make it so."
+    },
+    {
+        title: "Ownership",
+        icon: punch,
+        text: "Take back control of your content through the power of decentralized NFT technology. When you publish or purchase  content, it is safe inside the public blockchain."
+    },
+    {
+        title: "More Profits",
+        icon: moneyBag,
+        text: "Get more of your value back from your fans. We implement a competitive earnings model for our creators. We are not constricted by VISA and banks because we use cryptocurrency."
+    }
+];
 
 export default function Features() {
     return (
         <FeaturesContainer>
-            <FeatureCard title="NFT Encryption" icon={padlock} text="State of the art NFT encryption methods make sure that your purchase stays private. Unlike other platforms, the content is not publicy viewable unless you make it so." />
-            <FeatureCard title="Ownership" icon={punch} text="Take back control of your content through the power of decentralized NFT technology. When you publish or purchase  content, it is safe inside the public blockchain." />
-            <FeatureCard title="More Profits" icon={moneyBag} text="Get more of your value back from your fans. We implement a competitive earnings model for our creators. We are not constricted by VISA and banks because we use cryptocurrency." />
+            {
+                featuresData.map(({ title, icon, text }, index) => {
+                    return <FeatureCard title={title} icon={icon} text={text} key={index} />
+                })
+            }
         </FeaturesContainer>
     );
 };
