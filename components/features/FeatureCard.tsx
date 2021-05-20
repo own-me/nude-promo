4c341ebdeffe4441d23493711197b8419de3bc79
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-19 23:14:36 -0700

    add features images, add props, featuresCard

--- components/features/FeatureCard.tsx
+++ components/features/FeatureCard.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import styled from "styled-components";
 
 const CardContainer = styled.div`
-    
+    height: 500px;
+    width: 250px;
+    background-color: brown;
 `;
 
-export default function FeatureCard() {
+const Title = styled.h2`
+    color: white;
+`;
+
+const Icon = styled.img`
+    height: 100px;
+`;
+
+const Text = styled.div`
+    color: white;
+`;
+
+export default function FeatureCard({ title, icon, text }) {
     return (
         <CardContainer>
-            
+            <Title>{title}</Title>
+            <Icon src={icon} />
+            <Text>{text}</Text>
         </CardContainer>
     );
 };
--- components/features/Features.tsx
+++ components/features/Features.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import FeatureCard from "./FeatureCard";
+import padlock from "../../media/padlock.svg";
+import punch from "../../media/punch.svg";
+import moneyBag from "../../media/money-bag.svg";
 
 const FeaturesContainer = styled.div`
     min-height: 800px;
 `;
 
+
 export default function Features() {
     return (
         <FeaturesContainer>
-            
+            <FeatureCard title="NFT Encryption" icon={padlock} text="State of the art NFT encryption methods make sure that your purchase stays private. Unlike other platforms, the content is not publicy viewable unless you make it so." />
+            <FeatureCard title="Ownership" icon={punch} text="Take back control of your content through the power of decentralized NFT technology. When you publish or purchase  content, it is safe inside the public blockchain." />
+            <FeatureCard title="More Profits" icon={moneyBag} text="Get more of your value back from your fans. We implement a competitive earnings model for our creators. We are not constricted by VISA and banks because we use cryptocurrency." />
         </FeaturesContainer>
     );
 };
