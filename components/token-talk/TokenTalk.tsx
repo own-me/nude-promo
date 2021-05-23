dbd59cff81183e1db2ae6c6d2add723d43613a4a
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-05-22 23:27:34 -0700

    more token talk tuneup

--- components/token-talk/TokenTalk.tsx
+++ components/token-talk/TokenTalk.tsx
@@ -10,10 +10,18 @@ const TokenTalkHeader = styled.div`
     line-height: 2.0;
     letter-spacing: 2.0;
     color: #2A2825;
-    @media screen and (max-width: 900px) {
-        font-size: 50px;
+    @media screen and (max-width: 1200px) {
+        font-size: 40px;
     }
-    @media screen and (max-width: 600px) {
+`;
+
+const TokenTalkSubHeader = styled.div`
+    font-family: Yeseva One, Open Sans;
+    font-size: 40px;
+    line-height: 2.0;
+    letter-spacing: 2.0;
+    color: #2A2825;
+    @media screen and (max-width: 1200px) {
         font-size: 30px;
     }
 `;
@@ -24,6 +32,11 @@ const TokenTalkText = styled.div`
     margin: 50px 0px;
     font-family: Poppins,Open Sans;
     padding: 0px 50px;
+
+    @media screen and (max-width: 1200px) {
+        font-size: 25px;
+        padding: 0px 20px;
+    }
 `;
 
 
@@ -32,7 +45,7 @@ export default function TokenTalk() {
         <TokenTalkContainer>
             <TokenTalkHeader>$NUDE and $NUDENFT Tokens</TokenTalkHeader>
             <TokenTalkText>We will be launching an ERC-20 and ERC-721 token used for in-app transactions, NFT minting, governance, staking, investing and all that good tokenomics sauce we expect from crypto projects.</TokenTalkText>
-            <TokenTalkHeader style={{ fontSize: 40 }}>How are we unique?</TokenTalkHeader>
+            <TokenTalkSubHeader>How are we unique?</TokenTalkSubHeader>
             <TokenTalkText>Believe it or not, we are not a pump and dump.</TokenTalkText>
             <TokenTalkText>We will be deploying our tokens to mainnet only when we have a fully functioning app to use them with. This allows for proper auditing, testing and community input.</TokenTalkText>
             <TokenTalkText>Speculative investment is not our product pitch.</TokenTalkText>
