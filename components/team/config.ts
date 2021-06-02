8806f268b320042a14fa663585d89718792c1c1c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-01 20:33:22 -0700

    team config

---
+++ components/team/config.ts
@@ -0,0 +1,21 @@
+import chrisImage from "../../media/team/chris.jpg";
+import sylviaImage from "../../media/team/sylvia.jpg";
+import tobiImage from "../../media/team/tobi.jpg";
+
+export const teamData = [
+    {
+        name: "Christopher Trimboli",
+        image: chrisImage,
+        title: "CEO | Lead Web Engineer"
+    },
+    {
+        name: "Sylvia Makuch",
+        image: sylviaImage,
+        title: "Designer | Digital Artist"
+    },
+    {
+        name: "Tobi Trimboli",
+        image: tobiImage,
+        title: "Animal and Human Relations"
+    }
+];
