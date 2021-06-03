3286d133d0a6a609158beec82ff30fef034474bc
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-02 21:14:13 -0700

    add text padding funding section

--- components/funding/FundingText.tsx
+++ components/funding/FundingText.tsx
@@ -15,10 +15,10 @@ const FundingTextContainer = styled.div`
 const BoldText = styled.span`
     font-family: Yeseva One, Open Sans;
     display: block;
+    margin-bottom: 10px;
 
     @media screen and (max-width: 600px) {
         font-size: 25px;
-        margin-bottom: 10px;
     }
 `;
 
