f6868498c45b82e3dbae96a441698e64c074990f
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-02 19:34:39 -0700

    add funding config data

---
+++ components/funding/config.ts
@@ -0,0 +1,37 @@
+export const fundingTextData = [
+    {
+        boldText: "Current Funding Status:",
+        mainText: "Bootstraped and working other jobs."
+    },
+    {
+        boldText: "Funding Goals:",
+        mainText: "Get the team working full-time properly paid to speed up development, improve quality and hire more specialists."
+    },
+    {
+        boldText: "Why give us money?",
+        mainText: "There is billions of dollars in this emerging industry and we beleive Own Me can be a top player. We think we have a hot brand and a logical business model to generate profitable revenue."
+    },
+    {
+        boldText: "Pitch Deck or White Paper?",
+        mainText: "Working on it... want to help?"
+    }
+];
+
+export const fundingCardData = [
+    {
+        title: "Investors",
+        text: "We are looking for angel investment to grow the team and get everyone working full-time. Get in touch with us if your intrested in owning an early chunk of the company."
+    },
+    {
+        boldText: "VIP Patrons",
+        text: "We might set up a GitGrant, Patreon, GoFundMe account for Own Me and the team. Some perks would be: beta access, discord roles, merch, limited edition NFTs, early-adopter VIP badge on profile and more..."
+    },
+    {
+        boldText: "Wall of Fame",
+        text: "Patrons and Investors can be included in our Wall of Fame. Plug your company, twitter, name and leave a unique message. Wall of Fame members can get some perks."
+    },
+    {
+        boldText: "Celebrities",
+        text: "Have a large fanbase, following or are an established creator? We are open to doing VIP partner deals. We can give VIP support and promotion such as with NFT auctions and frontpage placements."
+    }
+];
