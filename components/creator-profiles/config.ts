09e9baa488bb865502925a44864044bcaf075aeb
Christopher Trimboli <christopher.trimboli@protonmail.com>
2021-06-05 23:24:03 -0700

    add creator profiles content config

---
+++ components/creator-profiles/config.ts
@@ -0,0 +1,22 @@
+export const cardData = [
+    {
+        title: "Private Subscriptions",
+        text: "Set a price for access to your private content. Fans and collectors have to pay a fixed crypto fee or stake a monthly amount of crypto in $NUDE on your channel to get access.",
+        color: "#FB33FF"
+    },
+    {
+        title: "Direct Messages",
+        text: "Chat directly with your fans and collectors. Charge them an amount of crypto to speak with you. Host time limited video calls and conversations.",
+        color: "#6833FF"
+    },
+    {
+        title: "NFT Auctions",
+        text: "Create, promote and manage your unique NFT auctions. Direct your fans from any platform to your auctions section. Make sure you get the most profit from your fans.",
+        color: "#FB33FF"
+    },
+    {
+        title: "Timeline",
+        text: "Post updates for your fans on a traditional social media timeline. Use this timeline to promote your NFT auctions and creator content. Get likes and shares to build your brand!",
+        color: "#0DC809"
+    },
+];
