e15c2f414d1baf218e7e774dedee2be642fa7e6c
Sylvia Makuch <sylvia.makuch@protonmail.com>
2021-10-29 19:06:36 -0700

    Finished Moderation Section

--- components/moderation/Moderation.tsx
+++ components/moderation/Moderation.tsx
@@ -3,7 +3,9 @@ import styled from "styled-components";
 import { moderationData } from "./config";
 import ModerationRow from "./ModerationRow";
 
+
 const ModerationContainer = styled.div`
+    z-index: 1;
 `;
 
 const ModerationHeader = styled.div`
@@ -11,14 +13,12 @@ const ModerationHeader = styled.div`
     font-size: 70px;
     line-height: 2.0;
     letter-spacing: 2.0;
-
-    @media screen and (max-width: 1200px) {
-        font-size: 45px;
-    }
-    @media screen and (max-width: 600px) {
+    padding:50px 0px;
+        @media screen and (max-width: 1200px) {
         font-size: 32px;
         text-align: center;
-    }
+        }
+
 `;
 
 export default function Moderation() {
@@ -27,6 +27,8 @@ export default function Moderation() {
             <ModerationHeader>
                 Moderation and Safety
             </ModerationHeader>
+        
+           
             {
                 moderationData.map(({ title, image, text, backgroundColor }, index) => {
                     return <ModerationRow 
@@ -38,6 +40,7 @@ export default function Moderation() {
                             />
                 })
             }
+            
         </ModerationContainer>
     );
 };
--- components/moderation/ModerationRow.tsx
+++ components/moderation/ModerationRow.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 
+
 const HiddenRow = styled.div`
     width:100%;
     display: flexbox;
     align-items: center;
     align-content: stretch; 
-    gap: 5.5em;
-    
-    `;
+    gap: 20%;
+
+    @media screen and (max-width: 1200px) {
+        flex-wrap: wrap-reverse ;
+        flex-direction: column-reverse;
+        align-items: center;
+        align-content: center;
+        justify-content: center;
+    }    
+`;
+
 const ModerationRowContainer = styled.div<{ backgroundColor: string }>`
     height: 200px;
     max-width: 50%;
     background-color: ${props => props.backgroundColor};
     padding: 30px;
     box-shadow: 0 3px 6px rgb(0 0 0 / 16%), 0 3px 6px rgb(0 0 0 / 23%);
-    margin: 30px;
-
-    @media screen and (max-width: 600px) {
-        margin: 20px 5px;
-    }
+    margin: 30px;   
+        @media screen and (max-width: 1200px) {
+        height: 400px;
+        max-width: 300px;
+        }
 `;
 
 const Title = styled.h2`
@@ -28,27 +37,40 @@ const Title = styled.h2`
     margin-bottom: 10px;
     font-size: 30px;
     font-family: Rock Salt, Open Sans;
+        @media screen and (max-width: 600px) {
+        font-size: 24px;
+        }
 `;
+
 const Text = styled.div`
     color: white;
     font-size: 20px;
     font-family: Poppins,Open Sans;
-
+    text-align: center;
+    @media screen and (max-width: 600px) {
+        margin-top: 20%;
+    }
 `;
+
 const ModImg = styled.img`
     width:200px ;
    justify-content:center;
+    @media screen and (max-width: 1200px) {
+        justify-content: center;
+    }
 `;
 export default function ModerationRow({ title, text, image, backgroundColor }) {
     return (
+
         <HiddenRow>
             <ModerationRowContainer backgroundColor={backgroundColor}>
-            <Title>{title}</Title>
-            <Text>{text}</Text>
+                <Title>{title}</Title>
+                <Text>{text}</Text>
             </ModerationRowContainer>
             <ModImg src={image} />
         </HiddenRow>
-        
+
+
 
     );
 };
