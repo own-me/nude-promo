ba7010fc481b05717ef8843ec6a7143ebee9990b
Sylvia Makuch <sylvia.makuch@protonmail.com>
2022-02-13 22:57:27 -0800

    new github svg added, and added to page

--- components/open-source/OpenSource.tsx
+++ components/open-source/OpenSource.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import OpenSourceText from "./OpenSourceText";
 import { openSourceData } from "./config";
-import bluebanner from "./../../media/bluebanner.svg"
+import github from "./../../media/github.svg"
+
 
 const PageContainer = styled.div`
- background-image: url(${bluebanner});
- height: 635px;
- padding: 80px;
+    padding: 100px;
+    display: flex;
+    flex-direction: row;
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const BlueBanner = styled.div`
+    background-color: #6AF6FF;
+    position: absolute;
+    height: 600px;
+    width: 120%;
+    z-index: -1;
+    transform: rotate(-8deg);
+    left:0px;
 `;
 
 const Title = styled.h2`
@@ -17,20 +34,30 @@ const Title = styled.h2`
     font-size: 30px;
 `;
 
-
 const Text = styled.div`
     color: black;
     font-size: 20px;
 `;
 
+const GitHub = styled.img`
+    height: 300px;
+    margin-left: 251px;
+`
+
 export default function OpenSource() {
     return (
         <PageContainer>
+            <BlueBanner />
+            <InnerContainer>
             {
                 openSourceData.map(({ title, text }, index) => {
-                    return <OpenSourceText title={title} text={text} key={index}/> 
+                    return <OpenSourceText title={title} text={text} key={index} />
                 })
             }
+            </InnerContainer>
+            <InnerContainer>
+            <GitHub src={github} />
+            </InnerContainer>
         </PageContainer>
     );
 };
