a82003b290b3dc78f2633fd2699e8334f49f0bbf
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-03-26 11:55:57 -0700

    add .svg to webpack resolver

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -37,7 +37,7 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: [".tsx", ".ts", ".js", ".css"],
+        extensions: [".tsx", ".ts", ".js", ".css", ".svg"],
     },
     plugins: [
         new HtmlWebpackPlugin({
